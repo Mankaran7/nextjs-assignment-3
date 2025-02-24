25eba850f8341592200968fcfade37578ddd7652
Mankaran Singh <mankaransingh22747@gmail.com>
2025-02-24 16:50:15 +0530

    third commit

--- app-router/my-app/app/recipe/page.js
+++ app-router/my-app/app/recipe/page.js
@@ -1,4 +1,4 @@
-"use client"
+/*"use client"
 import { useEffect, useState } from "react";
 import styles from './recipe.module.css'
 export default function Page(){
@@ -45,3 +45,71 @@ export default function Page(){
       )
      
 }*/
+"use client"
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Table = styled.table`
+  width: 100%;
+  margin: 20px 0;
+`;
+
+const TableHeader = styled.th`
+  background-color: green;
+  color: white;
+  padding: 10px;
+`;
+
+const TableCell = styled.td`
+  border: 1px solid lightgray;
+  padding: 8px;
+`;
+
+const TableRow = styled.tr`
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export default function Page() {
+    const [receipes, setreceipes] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/recipes")
+            .then((res) => res.json())
+            .then((data) => setreceipes(data.recipes.slice(0, 10)));
+    }, []);
+
+    return (
+        <>
+            <Title>Table of Recipes</Title>
+            <Table>
+                <thead>
+                    <tr>
+                        <TableHeader>ID</TableHeader>
+                        <TableHeader>Name</TableHeader>
+                        <TableHeader>Prep Time in Mins</TableHeader>
+                        <TableHeader>Servings</TableHeader>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        receipes.map((item) => (
+                            <TableRow key={item.id}>
+                                <TableCell>{item.id}</TableCell>
+                                <TableCell>{item.name}</TableCell>
+                                <TableCell>{item.prepTimeMinutes}</TableCell>
+                                <TableCell>{item.servings}</TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </tbody>
+            </Table>
+        </>
+    );
+}
--- next-tailwind/my-app/app/recipe/page.js
+++ next-tailwind/my-app/app/recipe/page.js
@@ -1,68 +1,20 @@
-"use client"
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-
-const Table = styled.table`
-  width: 100%;
-  margin: 20px 0;
-`;
-
-const TableHeader = styled.th`
-  background-color: green;
-  color: white;
-  padding: 10px;
-`;
-
-const TableCell = styled.td`
-  border: 1px solid lightgray;
-  padding: 8px;
-`;
-
-const TableRow = styled.tr`
-  &:hover {
-    background-color: lightgray;
-  }
-`;
-
-const Title = styled.h1`
-  text-align: center;
-  margin-bottom: 20px;
-`;
-
-export default function Page() {
-    const [receipes, setreceipes] = useState([]);
-
-    useEffect(() => {
-        fetch("https://dummyjson.com/recipes")
-            .then((res) => res.json())
-            .then((data) => setreceipes(data.recipes.slice(0, 10)));
-    }, []);
-
-    return (
-        <>
-            <Title>Table of Recipes</Title>
-            <Table>
-                <thead>
-                    <tr>
-                        <TableHeader>ID</TableHeader>
-                        <TableHeader>Name</TableHeader>
-                        <TableHeader>Prep Time in Mins</TableHeader>
-                        <TableHeader>Servings</TableHeader>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        receipes.map((item) => (
-                            <TableRow key={item.id}>
-                                <TableCell>{item.id}</TableCell>
-                                <TableCell>{item.name}</TableCell>
-                                <TableCell>{item.prepTimeMinutes}</TableCell>
-                                <TableCell>{item.servings}</TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </tbody>
-            </Table>
-        </>
-    );
+export default async function Page(){
+    const res = await fetch('https://dummyjson.com/recipes?limit=5').then((res) =>
+        res.json()
+      )
+      return(
+        <div className="flex gap-5 ">
+         {
+           res.recipes.map((item)=>(
+            <div key={item.id} className="mb-5 hover:bg-sky-700 ">
+           <p >{item.name}</p>
+            <p>Preperation Time {item.prepTimeMinutes}</p>
+            <p>Savings {item.servings}</p>
+        </div>
+        ))
+         }
+         <button className="focus:bg-blue-500">button</button>
+        </div>
+      )
+     
 }
